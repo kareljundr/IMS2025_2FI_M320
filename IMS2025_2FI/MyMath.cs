4380da82db7c8cf31bdea8d587d85be1ca01b2b3
Karel Drapal <kareljundrapal@gmail.com>
2025-08-18 09:32:11 +0200

    Projektdateien hinzufügen.

---
+++ IMS2025_2FI/MyMath.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS2025_2FI
+{
+    internal class MyMath
+    {
+        static internal int Add(int a, int b)
+        { 
+            return a + b; 
+        }
+            
+    }
+}
